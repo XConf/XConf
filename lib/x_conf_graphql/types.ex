e72d2d7bf02046cb4cdd8ceda2fa902fa22195d8
Mickey <poverwhelming@gmail.com>
2018-04-18 08:09:28 +0800

    added conference_schema.ex

--- lib/x_conf_graphql/types.ex
+++ lib/x_conf_graphql/types.ex
@@ -5,8 +5,11 @@ defmodule XConfGraphQL.Types do
   use Absinthe.Schema.Notation
   use Absinthe.Relay.Schema.Notation, :modern
 
+  import_types XConfGraphQL.Type.ConferenceSchema
+
   node object :conference do
     field :code, non_null(:string)
     field :name, non_null(:string)
+    field :speakers, list_of(:speaker)
   end
 end
---
+++ lib/x_conf_graphql/types/conference_schema.ex
@@ -0,0 +1,15 @@
+defmodule XConfGraphQL.Type.ConferenceSchema do
+  use Absinthe.Schema.Notation
+  use Absinthe.Relay.Schema.Notation, :modern
+
+  object :speaker do
+    field :id, non_null(:string)
+    field :name, non_null(:string)
+    field :title, non_null(:string)
+    field :pictureUrl, non_null(:string), resolve: &(&1.picture_url)
+    field :bio, :string
+    field :homepageUrl, :string, resolve: &(&1.homepage_url)
+    field :twitterUsername, :string, resolve: &(&1.twitter_username)
+    field :githubUsername, :string, resolve: &(&1.github_username)
+  end
+end
