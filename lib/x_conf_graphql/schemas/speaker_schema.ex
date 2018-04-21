412c59bcd61d55177ec6ae011d1895bb5d40fed9
Mickey <poverwhelming@gmail.com>
2018-04-21 16:36:11 +0800

    made title in speaker optional

--- lib/x_conf_graphql/schemas/speaker_schema.ex
+++ lib/x_conf_graphql/schemas/speaker_schema.ex
@@ -5,7 +5,7 @@ defmodule XConfGraphQL.SpeakerSchema do
   object :speaker do
     field :id, non_null(:string)
     field :name, non_null(:string)
-    field :title, non_null(:string)
+    field :title, :string
     field :pictureUrl, non_null(:string), resolve: &(&1.picture_url)
     field :bio, :string
     field :homepageUrl, :string, resolve: &(&1.homepage_url)
